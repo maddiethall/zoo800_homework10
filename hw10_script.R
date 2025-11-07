56be5c8411bde9ac0c2d19b2b13ae4139cdd2a51
maddiethall <methall@wisc.edu>
2025-11-07 11:44:39 -0600

    done!

--- hw10_script.R
+++ hw10_script.R
@@ -48,18 +48,73 @@ predictions
 ## OBJECTIVE 2
 
 set.seed(123)
-n = 100
-x = runif(n, 0, 50)
-beta0 = 5
-beta1 = 0.8
-sigma_true = beta0 + beta1 * x
-sd_log = 0.6
-sigma_log = 0
-eps_log = rlnorm(n, meanlog = sigma_log, sdlog = sd_log)
-center = exp(sigma_log + (sd_log^2) / 2)
-eps_lognorm_centered = eps_log - center
-y_lognorm = sigma_true + eps_lognorm_centered
-model_lognorm = lm(y_lognorm ~ x)
-summary(model_lognorm)
+X = runif(100, min = 0, max = 10)
+beta_0 = 2
+beta_1 = 3
+error = rlnorm(100, meanlog = 0, sdlog = 0.5)
+Y = beta_0 + beta_1 * X + error
+sim_df = data.frame(X = X, Y = Y)
+
+# plot original regression
+ggplot(sim_df, aes(x = X, y = Y)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE, color = "blue") +
+  labs(title = "Simulated Lognormal Data") +
+  theme_minimal()
+
+# creating a data frame to store results
+n_sim = 15
+coef_results = data.frame(
+  sim = integer(n_sim),
+  true_intercept = numeric(n_sim),
+  true_slope = numeric(n_sim),
+  est_intercept = numeric(n_sim),
+  est_slope = numeric(n_sim)
+)
+
+# creating a list for prediction intervals
+pred_intervals = vector("list", n_sim)
+
+# running simulations
+for (i in 1:n_sim) {
+  X2 = runif(100, min = 0, max = 10)
+  beta2_0 = runif(1, 0, 5)
+  beta2_1 = runif(1, 1, 4)
+  error2 = rlnorm(100, meanlog = 0, sdlog = 0.5)
+  Y2 = beta2_0 + beta2_1 * X2 + error2
+  sim_model = lm(Y2 ~ X2)
+  coef_results[i, ] = c(i, beta2_0, beta2_1, coef(sim_model)[1], coef(sim_model)[2])
+  new_data = data.frame(X = X2)
+  pred_int = predict(sim_model, 
+                     newdata = new_data, 
+                     interval = "prediction", 
+                     level = 0.95)
+  pred_intervals[[i]] = data.frame(X = X2, Y = Y2, pred_int)
+}
+
+# viewing coefficient results
+head(pred_intervals[[1]])   
+
+# comparing estimated and true values
+coef_results$slope_error = coef_results$est_slope - coef_results$true_slope
+coef_results$intercept_error = coef_results$est_intercept - coef_results$true_intercept
+
+summary(coef_results$slope_error)
+summary(coef_results$intercept_error)
+# Slope: estimates are fairly accurate, with mean error close to 0 (0.05), so a slight upward bias
+# Intercept: estimates are less accurate, with a larger mean error (1.11), so a consistent upward bias  
+
+fraction_95 = sapply(pred_intervals, function(df) {
+  within = df$Y >= df$lwr & df$Y <= df$upr
+  mean(within)
+})
+coef_results$fraction_95 = fraction_95
+summary(coef_results$fraction_95)
+# On average, about 95.33% of the observed Y values fall within the 95% prediction intervals!
+# The whole range is between 93% and 97%, so a fairly limited variability
+# meaning that our model's predictions intervals are pretty accurate!
+# Even with a non-normal error, the linear model is robust enough!
+
+
 
 
